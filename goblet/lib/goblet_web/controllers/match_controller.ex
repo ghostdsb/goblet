e0fcfb674621823ad7b809f7b7c890d9cab1318b
dibyanshu <dibyanshu@gamezop.co>
2020-09-12 19:51:05 +0530

    removed api calls

--- goblet/lib/goblet_web/controllers/match_controller.ex
+++ goblet/lib/goblet_web/controllers/match_controller.ex
@@ -1,6 +1,6 @@
 defmodule GobletWeb.MatchController do
   use GobletWeb, :controller
-  alias Goblet.MatchFunction
+  # alias Goblet.MatchFunction
   require Logger
 
 
@@ -13,23 +13,25 @@ defmodule GobletWeb.MatchController do
   """
   def get_match(conn, params) do
     Logger.info(" conn-> #{inspect(conn)}, params -> #{inspect(params)}")
-    match_data = MatchFunction.find_match(%{
-      "player_id" => params["player_id"],
-      "room_name" => params["room_name"],
-      "player_count" => params["player_count"] |> String.to_integer,
-    })
+    match_data = ""
+    # MatchFunction.find_match(%{
+    #   "player_id" => params["player_id"],
+    #   "room_name" => params["room_name"],
+    #   "player_count" => params["player_count"] |> String.to_integer,
+    # })
     json(conn, match_data)
   end
 
 
   def get_by_id(conn, params) do
     Logger.info(" conn-> #{inspect(conn)}, params -> #{inspect(params)}")
-    match_data = MatchFunction.find_match_by_id(%{
-      "player_id" => params["player_id"],
-      "room_name" => params["room_name"],
-      "match_id"  => params["match_id"],
-      "player_count" => params["player_count"] |> String.to_integer,
-    })
+    match_data = ""
+    #   MatchFunction.find_match_by_id(%{
+    #   "player_id" => params["player_id"],
+    #   "room_name" => params["room_name"],
+    #   "match_id"  => params["match_id"],
+    #   "player_count" => params["player_count"] |> String.to_integer,
+    # })
     json(conn, match_data)
   end
 end
